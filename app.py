573ecfa87ad17286daa81addfda1b166a49b6518
dragost305-web <dragost305@gmail.com>
2026-07-06 11:50:53 +0530

    Update app.py v01

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ st.write("This Streamlit app is running in a Docker container on Azure!")
 
 name = st.text_input("What's your name?")
 if name:
-    st.success(f"Nice to meet you, {name}!")
+    st.success(f"Nice to meet you, {name}!, hope you are doing well")
