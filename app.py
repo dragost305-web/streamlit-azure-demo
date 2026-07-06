189e8718b80c052c3ba389fa9793d81e8f501f44
dragost305-web <dragost305@gmail.com>
2026-07-06 10:15:54 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello from Azure App Service 👋")
+st.write("This Streamlit app is running in a Docker container on Azure!")
+
+name = st.text_input("What's your name?")
+if name:
+    st.success(f"Nice to meet you, {name}!")
